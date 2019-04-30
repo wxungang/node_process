0e4c19b65f88cbc7d1ea903d36f7d272b2f7e7be
wangxungang <wxungang@163.com>
2019-04-30 14:52:18 +0800

    cluster done

--- src/main/app.js
+++ src/main/app.js
@@ -2,14 +2,18 @@ const service = require('../service/index');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
-let startNum = 100;
-let num = 0;
 
-setInterval(() => {
-    // service.func(num++)
-}, 5);
+// 定时器
+function timer(step) {
+    let startNum = 100;
+    let num = 0;
+    setInterval(() => {
+        service.func(startNum + num++ * numCPUs + step)
+    }, 500);
+}
 
 
+// 入口
 if (cluster.isMaster) {
     console.log(`主进程 ${process.pid} 正在运行`);
 
@@ -23,10 +27,7 @@ if (cluster.isMaster) {
     });
 } else {
     console.log(cluster.worker.id);
-    // 利用 cpu 的多核性能
-    setInterval(() => {
-        service.func(startNum + num++ * numCPUs + cluster.worker.id)
-    }, 500);
-
     console.log(`工作进程 ${process.pid} 已启动`);
+    // 利用 cpu 的多核性能
+    timer(cluster.worker.id);
 }
